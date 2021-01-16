6dec814c34ac876f3b885c39e0746add0c0030af
AntonC9018 <anton.curm@gmail.com>
2021-01-17 00:52:49 +0200

    this should have been a 1

--- assets/shader_src/grid.fs
+++ assets/shader_src/grid.fs
@@ -6,5 +6,5 @@ void main()
     if (myCoord.x * myCoord.y < 0)
         gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
     else
-        gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);
+        gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
 }
